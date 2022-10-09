3bc2dd8f8572eaf331577145beef51d1cc275ec1
zairemoon <tor.devries@wsu.edu>
2022-10-09 04:40:30 +0000

    updated with AJAX calls and timeout function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,40 @@
-// JavaScript for TP5
+// JavaScript for TP6
+
+// function to load a file from the URL "fromFile" into the object identified by "whereTo'
+function loadFileInto(fromFile, whereTo) {
+
+  // creating a new XMLHttpRequest object
+  ajax = new XMLHttpRequest();
+
+  // defines the GET/POST method, source, and async value of the AJAX object
+  ajax.open("GET", fromFile, true);
+
+  // provides code to do something in response to the AJAX request
+  ajax.onreadystatechange = function() {
+    if ((this.readyState == 4) && (this.status == 200)) {
+      document.querySelector(whereTo).innerHTML += this.responseText;
+
+    } else if ((this.readyState == 4) && (this.status != 200)) {
+      console.log("Error: " + this.responseText);
+    }
+
+  } // end ajax.onreadystatechange function
+
+  // initiate request and wait for response
+  ajax.send();
+
+}
+
+
 
 
 window.onload = function() {
+
+  loadFileInto("ingredients.html", "#ingredients ul");
+  loadFileInto("equipment.html", "#equipment ul");
+  loadFileInto("directions.html", "#directions ol");
+
+
 
   document.querySelector("#titleBanner h1").classList.add("tp");
   document.querySelector("#titleBanner h1").onclick = function() {
@@ -19,8 +52,9 @@ window.onload = function() {
   document.querySelector("#recipeColumns #directions").onclick = function() {
     this.classList.toggle("tpShow");
   }
-
+  
+  setTimeout(function() {
   document.querySelector("#recipeColumns #equipment ul").innerHTML += "<li>A hunger for dessert</li>";
-
+  }, 100);
 
 } // end window.onload
